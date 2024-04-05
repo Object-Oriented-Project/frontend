aaac2c101ffb6b59a39543159b657fb27e53d628
dorf0-0 <6434491223@student.chula.ac.th>
2024-04-05 10:24:44 +0700

    menupage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,38 @@
+import Slideshow from "./slideshow/page";
+import Navbar from "./components/navbar";
+import Banner from "./components/banner";
+import Menu from "./components/menupage";
+
+
 export default function Home() {
-  return <div className="btn btn-primary">hello world</div>;
+  return <div>
+    {/* <Slideshow /> */}
+ 
+    <Navbar />
+    <Banner />
+    <Menu />
+    {/*  <Footer/> */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  </div>
+
+
 }
