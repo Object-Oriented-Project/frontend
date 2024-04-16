72945acb9e986ebec10a8010894123b7bf53f26c
Wer1d <98684652+Wer1d@users.noreply.github.com>
2024-04-16 13:15:49 +0700

    feat: create Customer obj and add product in Order obj

--- src/app/components/Customer.tsx
+++ src/app/components/Customer.tsx
@@ -1,7 +1,17 @@
-class Customer {
-    constructor(name, tableNo) {
-        this.name = name;
-        this.tableNo = tableNo;
+export default class Customer {
+    #name: string; // # is a private field => access with dot is not allowed
+    #uid: string;
+    
+    constructor(name: string) {
+        this.#name = name;
+        this.#uid = Math.random().toString(36).substring(5);
+    }
+    public getName(): string {
+        return this.#name;
+    }
+
+    public getUid(): string {
+        return this.#uid;
     }
 
 }
