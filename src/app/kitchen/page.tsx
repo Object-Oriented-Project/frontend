0b9ae73dffe2c2e80f45c0dacddb371ee3fc3202
AK Katung <6434496423@student.chula.ac.th>
2024-04-17 17:24:04 +0700

    feat: socket connected

--- src/app/kitchen/page.tsx
+++ src/app/kitchen/page.tsx
@@ -1,19 +1,16 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TableMap from "../components/TableMap";
 
 export default function KitchenPage() {
   const [filterStatus, setFilterStatus] = useState("All");
-  const [orders, setOrders] = useState([
-    { id: 1, name: "Cookie", status: "Ongoing" },
-    { id: 2, name: "Noodle", status: "Ongoing" },
-  ]);
+  const [orders, setOrders] = useState([]);
   const [expandedOrderId, setExpandedOrderId] = useState(null);
 
   const filterOrders = (orders, status) => {
     return status === "All"
       ? orders
-      : orders.filter((order) => order.status === status);
+      : orders.filter((order) => order.order_status === status);
   };
 
   const updateOrderStatus = (orders, orderId, newStatus) => {
@@ -37,6 +34,23 @@ export default function KitchenPage() {
     setExpandedOrderId(expandedOrderId === orderId ? null : orderId);
   };
 
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:3001/api/kitchen/ws/kitchen");
+    socket.onopen = () => {
+      console.log("Connected to the kitchen websocket server");
+    };
+    socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      console.log("Received data from the kitchen websocket server", data);
+      //append the new order to the orders array
+      setOrders((orders) => [...orders, data]);
+    };
+    return () => {
+      socket.close();
+    };
+  }
+  , []);
+
   return (
     <div>
       <div> Your Job</div>
@@ -55,7 +69,7 @@ export default function KitchenPage() {
         </button>
         <button
           className="btn btn-primary"
-          onClick={() => handleFilterClick("Ongoing")}
+          onClick={() => handleFilterClick("ONGOING")}
         >
           Ongoing
         </button>
@@ -67,30 +81,30 @@ export default function KitchenPage() {
             className="container sm mb-4 bg-red-200 flex flex-col ml-5 w-80"
           >
             <div className="flex flex-row justify-between items-center rounded-md bg-blue-300">
-              <span className="font-semibold">{order.name}</span>
+              <span className="font-semibold">{order.customer_name}</span>
               <p>
                 <select
                   className="select select-bordered w-full max-w-xs"
-                  value={order.status}
+                  value={order.order_status}
                   onChange={(e) => handleStatusChange(order.id, e.target.value)}
                 >
                   <option value="Done">Done</option>
-                  <option value="Ongoing">Ongoing</option>
+                  <option value="ONGOING">Ongoing</option>
                 </select>
               </p>
               <button
                 className=" bg-gray-200 text-left p-2 mb-2"
-                onClick={() => toggleOrderDetails(order.id)}
+                onClick={() => toggleOrderDetails(order.order_id)}
               >
                 <span>{expandedOrderId === order.id ? "-" : "+"}</span>
               </button>
             </div>
-            {expandedOrderId === order.id && (
+            {expandedOrderId === order.order_id && (
               <div className="bg-gray-100 flex flex-col mt-2">
                 <div>
-                  <div>Order ID: {order.id}</div>
-                  <div>Status: {order.status}</div>
-                  <div>Order Name: {order.name}</div>
+                  <div>Order ID: {order.order_id}</div>
+                  <div>Status: {order.order_status}</div>
+                  <div>Order Name: {order.customer_name}</div>
                 </div>
                 {/* Add additional order details here */}
               </div>
